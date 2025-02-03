5a73c4ff49046019a6113e4653086d005dd751c0
Anna Verkhovskaya <annaverpcs@gmail.com>
2025-02-03 14:46:11 -0800

    AnnaV

--- steps/steps.py
+++ steps/steps.py
@@ -6,7 +6,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 # from selenium.webdriver.common.keys import Keys
 
-import api
+#import api
 
 
 @step('Open "{url}"')
@@ -83,46 +83,6 @@ def create_project(context):
         click_element(context, f"{elements['duration']}{row[4]}']")
 
 
-@step("Fill out following information with keys")
-def create_project_keys(context):
-    elements = {
-        'Project Name': "//div[./label[text()='Project Name']]//input",
-        'Start date': "//input[@placeholder='Start date']",
-        'Project description': "//div[./label[text()='Project description']]//textarea",
-        'Period Dimension': "//span[text()='Period Dimension']",
-        'Project Duration': "//div[./label[text()='Project Duration']]//span[text()='",
-    }
-    for row in context.table:
-        if row.cells[0] in ['Project Name', 'Start date', 'Project description']:
-            type_text(context, row.cells[1], elements[row.cells[0]])
-        elif row.cells[0] == 'Period Dimension':
-            click_element(context, elements[row.cells[0]])
-            sleep(0.5)
-            click_element(context, f"//li/span[text()='{row.cells[1]}']")
-        else:
-            click_element(context, f"{elements[row.cells[0]]}{row.cells[1]}']")
-
-
-@step("Login with {user} credentials")
-def login(context, user):
-    users = {
-        'Admin': ('pcs.automationclass@gmail.com', 'Qwerty7'),
-        'Sale': ('pcs.automationclass+1@gmail.com', 'Qwerty7+1'),
-        'Wrong': ('pcs.automationclass@gmail.com', 'Qwerty'),
-        'Manager': ('pcs.automationclass+4@gmail.com', 'Qwerty7'),
-    }
-    click_element(context, "//a[text()='Login']")
-    type_text(context, users[user][0], "//input[@name='username']")
-    type_text(context, users[user][1], "//input[@name='password']")
-    click_element(context, "//button[contains(text(), 'Login')]")
-
-
-@step('Get weather in "{city}"')
-def get_weather(context, city):
-    weather = api.get_weather(city)
-    print(weather)
-
-
 @step("Switch to new window")
 def switch_to(context):
     all_windows = context.driver.window_handles
@@ -137,3 +97,22 @@ def switch_to_home(context):
 @step("Close current window")
 def step_impl(context):
     context.driver.close()
+
+
+@step('Verify that xpath "{xpath}" should contain text "{expected_text}"')
+def step_impl(context, xpath, expected_text):
+      #element = WebDriverWait(context.driver, 10).until(EC.presence_of_element_located((By.XPATH, xpath)))
+      element =  context.driver.find_element(By.XPATH, xpath)
+      actual_text = element.text
+      print(f"Expected text: {expected_text}")
+      print(f"Actual text: {actual_text}")
+      # Assert the text matches exactly
+      assert actual_text == expected_text, f"Text does not match: '{actual_text}' != '{expected_text}'"
+      # Optionally assert that the expected text is contained in the actual text
+      assert expected_text in actual_text, f"Expected text '{expected_text}' not found in '{actual_text}'"
+
+
+
+@step('Wait for the element with xpath "{xpath}" to be present')
+def step_impl(context, xpath):
+    WebDriverWait(context.driver, 20).until(EC.presence_of_element_located((By.XPATH, xpath)))
